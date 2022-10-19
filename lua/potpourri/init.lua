53aaf4a68b52c1eeeb996250311179b5f7572c98
flo <f@linguenheld.fr>
2022-10-19 21:26:35 +0200

    Add python

--- lua/potpourri/init.lua
+++ lua/potpourri/init.lua
@@ -2,12 +2,15 @@ local A = vim.api
 local map = A.nvim_set_keymap
 
 local RP = require('potpourri.replace')
+local PY = require('potpourri.python')
 
 
 
-local function mange()
+function mange()
     print('Mange ton chien !')
 end
+
+
 
 
 --------------------------------------------------------------------------------------------------
@@ -17,5 +20,9 @@ end
 local map_opt = {}
 
 map('n', '<leader>mtc', ':lua mange()<cr>', map_opt)
+
 map('n', '<leader>ppr', ':lua RP.replace_parenthesis()<cr>', map_opt)
 map('n', '<leader>pqr', ':lua RP.replace_quote()<cr>', map_opt)
+
+map('n', '<leader>p(',  ':lua PY.close_python_function(true)<cr>', map_opt)
+map('n', '<leader>p)',  ':lua PY.close_python_function(false)<cr>', map_opt)
